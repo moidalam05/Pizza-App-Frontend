785926a33d6217e863d7b873bf6ab8523497153d
moid05 <moidalam005@hotmail.com>
2025-01-02 16:41:05 +0530

    Add authentication routes and NotFound page to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Home from "./Pages/Home";
 import { Routes, Route } from "react-router-dom";
+import Signup from "./Pages/Auth/Signup";
+import Login from "./Pages/Auth/Login";
+import NotFound from "./Pages/NotFound";
 
 const App = () => {
 	return (
@@ -9,6 +12,18 @@ const App = () => {
 				<Route
 					path="/"
 					element={<Home />}
+				/>
+				<Route
+					path="/auth/signup"
+					element={<Signup />}
+				/>
+				<Route
+					path="/auth/login"
+					element={<Login />}
+				/>
+				<Route
+					path="*"
+					element={<NotFound />}
 				/>
 			</Routes>
 		</>
