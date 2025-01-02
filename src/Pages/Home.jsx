3f00cb4225a3fe30767fa9b58cfe23c96ad42e5c
moid05 <moidalam005@hotmail.com>
2025-01-02 14:17:02 +0530

    Add services section to Home component with cooking benefits

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { FaArrowRightLong } from "react-icons/fa6";
+import { BsPatchCheck } from "react-icons/bs";
 import pizzaImage from "../assets/Images/pizza2.png";
+import cookImage from "../assets/Images/cooking1.png";
 
 const Home = () => {
 	return (
@@ -31,9 +33,83 @@ const Home = () => {
 					</button>
 				</div>
 
-                <div>
-                    <img src={pizzaImage} alt="pizza" width={550} height={550} />
-                </div>
+				<div>
+					<img
+						src={pizzaImage}
+						alt="pizza"
+						width={550}
+						height={550}
+					/>
+				</div>
+			</section>
+
+			{/* Services section */}
+			<section className="py-4 mt-6 bg-gradient-to-r from-orange-400 to-amber-50">
+				<div className="container flex flex-col md:flex-row">
+					<div className="flex flex-col items-center justify-center rounded-lg lg:w-1/2">
+						<img
+							src={cookImage}
+							alt="cooking"
+							width={500}
+							className="rounded-lg"
+						/>
+					</div>
+
+					<div className="flex flex-col flex-wrap text-center lg:py-6 lg:w-1/2 lg:pl-12 lg:text-left">
+						<div className="flex flex-col items-center lg:items-start">
+							<div>
+								<h2 className="mb-2 text-5xl font-extrabold text-transparent bg-gradient-to-r from-orange-600 to-orange-300 bg-clip-text">
+									Cooked by the best <br /> chefs in the world
+								</h2>
+								<p className="text-base leading-relaxed text-[#687280]">
+									There are many benefits regarding to that
+									but the main ones are:
+								</p>
+							</div>
+						</div>
+
+						<div className="w-full p-1">
+							<div className="flex items-center h-full p-2 text-2xl rounded">
+								<BsPatchCheck className="text-[#F38339] w-10 h-10 mr-4" />
+								<span className="font-bold title-font">
+									Perfect taste
+								</span>
+							</div>
+						</div>
+						<div className="w-full p-1">
+							<div className="flex items-center h-full p-2 text-2xl rounded">
+								<BsPatchCheck className="text-[#F38339] w-10 h-10 mr-4" />
+								<span className="font-bold title-font">
+									Prepared Quickly
+								</span>
+							</div>
+						</div>
+						<div className="w-full p-1">
+							<div className="flex items-center h-full p-2 text-2xl rounded">
+								<BsPatchCheck className="text-[#F38339] w-10 h-10 mr-4" />
+								<span className="font-bold title-font">
+									Food heygiene guaranteed
+								</span>
+							</div>
+						</div>
+						<div className="w-full p-1">
+							<div className="flex items-center h-full p-2 text-2xl rounded">
+								<BsPatchCheck className="text-[#F38339] w-10 h-10 mr-4" />
+								<span className="font-bold title-font">
+									Fresh Ingredients
+								</span>
+							</div>
+						</div>
+						<div className="w-full p-1">
+							<div className="flex items-center h-full p-2 text-2xl rounded">
+								<BsPatchCheck className="text-[#F38339] w-10 h-10 mr-4" />
+								<span className="font-bold title-font">
+									Guaranteed Satisfaction!
+								</span>
+							</div>
+						</div>
+					</div>
+				</div>
 			</section>
 		</div>
 	);
