44e63881f3b4de72412dd644bd3349df52980e97
moid05 <moidalam005@hotmail.com>
2025-01-13 15:53:54 +0530

    Enhance Layout component with logout functionality and navigation to home

--- src/Layouts/Layout.jsx
+++ src/Layouts/Layout.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Pizzalogo from "../assets/Images/pizza1.png";
 import Footer from "../Components/Footer";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { logout } from "../Redux/Slices/AuthSlice";
 
 const Layout = ({ children }) => {
 	const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
 	const dispatch = useDispatch();
+	const navigate = useNavigate();
+
 	async function handleLogout(e) {
 		e.preventDefault();
 		dispatch(logout());
@@ -15,7 +17,10 @@ const Layout = ({ children }) => {
 	return (
 		<div>
 			<nav className="flex items-center justify-around h-16 text-[#6B7280] font-mono border-none shadow-md">
-				<div className="flex items-center justify-center">
+				<div
+					onClick={() => navigate("/")}
+					className="flex items-center justify-center"
+				>
 					<p>Pizza App</p>
 					<img
 						src={Pizzalogo}
