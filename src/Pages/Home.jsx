f1c2dc18bf01abad21d2ed05365b0847ef53fbd8
moid05 <moidalam005@hotmail.com>
2025-01-13 16:48:59 +0530

    Add product links to Home component for detailed view

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,6 +9,7 @@ import enjoyFood from "../assets/Images/enjoy.png";
 import pickupFood from "../assets/Images/pickup.png";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllProducts } from "../Redux/Slices/ProductSlice";
+import { Link } from "react-router-dom";
 
 const Home = () => {
 	const dispatch = useDispatch();
@@ -173,6 +174,45 @@ const Home = () => {
 						</div>
 					</div>
 				</section>
+
+				<div className="mx-auto">
+					<div className="flex flex-wrap justify-center">
+						{productsData.map((item) => {
+							return (
+								item.inStock && (
+									<div
+										className="p-4 md:w-1/3"
+										key={item._id}
+									>
+										<Link to={`/product/${item._id}`}>
+											<div className="overflow-hidden border rounded-lg border-opacity-60">
+												<img
+													src={item.productImage}
+													alt="Pizza Image"
+													className="object-cover object-center w-full lg:h-48 md:h-36"
+												/>
+												<div className="p-6 border">
+													<h2 className="text-xs font-medium tracking-widest text-gray-400 title-font">
+														{item.category}
+													</h2>
+													<h1 className="mb-3 text-lg font-medium text-gray-900 title-font">
+														{item.productName}
+													</h1>
+													<p className="mb-4 text-base leading-relaxed">
+														{item.description}
+													</p>
+													<p className="text-lg font-medium text-gray-900 title-font">
+														${item.price}
+													</p>
+												</div>
+											</div>
+										</Link>
+									</div>
+								)
+							);
+						})}
+					</div>
+				</div>
 			</div>
 		</Layout>
 	);
